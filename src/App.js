0ab9b58aa29cbd03964239de4550a3f701258ba6
aliu19 <liu.and@northeastern.edu>
2021-03-25 02:13:47 -0400

    Created home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import CourseManager from "./components/course-manager/course-manager";
 import CourseEditor from "./components/course-editor/course-editor";
+import Home from "./components/home";
 import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      {/*<Route path="/" component={Home}/>*/}
+      <Route path="/" component={Home} exact={true}/>
       <Route path="/courses" component={CourseManager}/>
       <Route path="/editor" render={(props) => <CourseEditor {...props}/>}/>
       {/*passes down children of props - spreader operator*/}
---
+++ src/components/home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const Home = () =>
+    <div>
+      <h1>Home</h1>
+      <div className="list-group">
+        <Link to="/courses/table" className="list-group-item">
+          Courses Table
+        </Link>
+        <Link to="/courses/grid" className="list-group-item">
+          Courses Grid
+        </Link>
+        <Link to="/editor" className="list-group-item">
+          Courses Editor
+        </Link>
+      </div>
+    </div>
+
+export default Home
