d2c835bbb772b0f10345f5adbaebc1816ff446b9
aliu19 <liu.and@northeastern.edu>
2021-04-26 14:15:28 -0400

    Render quiz name

--- src/components/quizzes/quiz.js
+++ src/components/quizzes/quiz.js
@@ -1,23 +1,29 @@
 import React, {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
 import questionService from "../../services/questions-service"
+import quizService from "../../services/quizzes-service"
 import Question from "./questions/question";
 
 const Quiz = () => {
 
   const {quizId} = useParams()
   const [questions, setQuestions] = useState([])
+  const [quizTitle, setQuizTitle] = useState("")
 
   useEffect(() => {
     questionService.findQuestionsForQuiz(quizId)
     .then((questions) => {
       setQuestions(questions)
     })
+    quizService.findQuizById(quizId)
+    .then((quiz) => {
+      setQuizTitle(quiz.title)
+    })
   }, [])
 
   return (
       <div>
-        <h1>Quiz TODO QUIZNAME</h1>
+        <h1>{quizTitle}</h1>
         <div className="list-group">
           {
             questions.map((question) => {
